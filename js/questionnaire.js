623a6ac7e214f3f927a195a3d16d36b8a832a998
lauragyr <laura.gyr@hotmail.com>
2019-05-17 23:49:33 +0200

    started working on question-array-switch

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -18,3 +18,32 @@ function questionLoop() {
 for(let i=0; i < question.length; i++) {
 }
 }
+
+//start-button
+let startButton = document.getElementById("start");
+let startScreen = document.getElementById("homescreen");
+let questionBox1 = document.getElementById("questionBox1");
+let questionBox2 = document.getElementById("questionBox2");
+let questionBox3 = document.getElementById("questionBox3");
+let questionBox4 = document.getElementById("questionBox4");
+let questionBox5 = document.getElementById("questionBox5");
+let questionBox6 = document.getElementById("questionBox6");
+let questionBox7 = document.getElementById("questionBox7");
+let questionBox8 = document.getElementById("questionBox8");
+let questionBox9 = document.getElementById("questionBox9");
+let questionBox10 = document.getElementById("questionBox10");
+let questionBox11 = document.getElementById("questionBox11");
+let questionBox12 = document.getElementById("questionBox12");
+let questionBox13 = document.getElementById("questionBox13");
+let questionBox14 = document.getElementById("questionBox14");
+let questionBox15 = document.getElementById("questionBox15");
+let questionBox16 = document.getElementById("questionBox16");
+let questionBox17 = document.getElementById("questionBox17");
+
+let questionSwitch = [questionBox1, questionBox2, questionBox3, questionBox4, questionBox5, questionBox6, questionBox7, questionBox8, questionBox9, questionBox10, questionBox12, questionBox13, questionBox14, questionBox15, questionBox16, questionBox17];
+
+document.getElementById("start").addEventListener("click", function(){
+    questionBox1.style.display = "block";
+    startScreen.style.display = "none";
+    startButton.className = "activeButton";
+  });
