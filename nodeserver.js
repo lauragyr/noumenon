6130167bedfe03d75f84c2cda0eb63fbaab78906
lauragyr <laura.gyr@hotmail.com>
2019-05-18 20:20:05 +0200

    tested nodeserver and json-file

--- nodeserver.js
+++ nodeserver.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 
 io.on('connection', client => {
-    client.on('answers', (a1, a2, a3, a4, a5, a6) => {
+    client.on('answers', (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17) => {
         let data2file = {
             "antwort1": a1,
             "antwort2": a2,
@@ -13,6 +13,17 @@ io.on('connection', client => {
             "antwort4": a4,
             "antwort5": a5,
             "antwort6": a6,
+            "antwort7": a7,
+            "antwort8": a8,
+            "antwort9": a9,
+            "antwort10": a10,
+            "antwort11": a11,
+            "antwort12": a12,
+            "antwort13": a13,
+            "antwort14": a14,
+            "antwort15": a15,
+            "antwort16": a16,
+            "antwort17": a17,
         };
         
         fs.writeFile("data.json", JSON.stringify(data2file), function (err) {
