994ed03e6a3ced67dcb0931e65e06ef59c9daa22
lauragyr <laura.gyr@hotmail.com>
2019-05-21 22:47:34 +0200

    added print and end function

--- nodeserver.js
+++ nodeserver.js
@@ -2,8 +2,6 @@ const server = require('http').createServer();
 const io = require('socket.io')(server);
 const fs = require('fs');
 
-
-
 io.on('connection', client => {
     client.on('answers', (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17) => {
         let data2file = {
@@ -32,6 +30,16 @@ io.on('connection', client => {
             }
             console.log("writing to file: " + JSON.stringify(data2file));
         });
+    });
+
+    client.on('print', () => {
+
+        console.log("boi")
+    });
+
+    client.on('end', () => {
+        
+        console.log("boi")
     });
 
     client.on('disconnect', () => {
