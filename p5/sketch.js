5e5af687a97ad9ae40dadf4f270b2f2a74effe1f
lauragyr <laura.gyr@hotmail.com>
2019-05-23 18:02:59 +0200

    simon cleaned my sketch

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,29 +1,51 @@
 //data-stuff
 let gegebenAntworten;
 let data;
+let surveyData = [];
+let possibleParameters;
+//let a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17;
 
-let a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17;
+possibleParameters = {}
+function resetAllParameters()
+{
+  possibleParameters =
+  {
+    colorR: 0,
+    colorG: 0,
+    colorB: 0,
+    angle: 1,
+    generalAngle: 5.4,
+    pos: 120,
+    npoints: 25,
+    radius: 50,
+    distance: 0.15,
+    a: 50
+  }
+}
+resetAllParameters();
 
+/*
 //canvas-stuff
-let angle = 1;
+let angle = 1; //angle of form(polygon)
 let generalAngle = 5.4; //start of circle draw
 
 //polygon-stuff
-let pos = 80;
-
-let npoints = 10;
-let radius = 10;
-let distance = 0.1;
+let pos = 120; //anordnung im kreis (aussen oder innen)
+let npoints = 25;
+let radius = 50;
+let distance = 0.15;
 
 //color-stuff
 let colorR = 0;
 let colorG = 0;
 let colorB = 0;
-let a = 100;
+let a = 50;
+//let filter = blendMode(BLEND);
+*/
 
 //video-stuff
-var ctracker;
-var videoInput
+let ctracker;
+let videoInput
 
 //--------------//
 
@@ -31,7 +53,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   noStroke();
-  fill(colorR, colorG, colorB, a);
+  //fill(colorR, colorG, colorB, a);
 
   /*videoInput = createCapture(VIDEO);
   videoInput.size(800, 600);
@@ -47,33 +69,54 @@ function setup() {
 
 // which one is the last object
 function drawData(data) {
-
   if (gegebenAntworten != Object.keys(data).length) {
     gegebenAntworten = Object.keys(data).length;
-    let lastAnswer = data[Object.keys(data)[gegebenAntworten - 1]];
+    //let lastAnswer = data[Object.keys(data)[gegebenAntworten - 1]];
   }
-  a1 = data.antwort1;
-  a2 = data.antwort2;
-  a3 = data.antwort3;
-  a4 = data.antwort4;
-  a5 = data.antwort5;
-  a6 = data.antwort6;
-  a7 = data.antwort7;
-  a8 = data.antwort8;
-  a9 = data.antwort9;
-  a10 = data.antwort10;
-  a11 = data.antwort11;
-  a12 = data.antwort12;
-  a13 = data.antwort13;
-  a14 = data.antwort14;
-  a15 = data.antwort15;
-  a16 = data.antwort16;
-  a17 = data.antwort17;
+
+  surveyData = [
+    data.antwort1,
+    data.antwort2,
+    data.antwort3,
+    data.antwort4,
+    data.antwort5,
+    data.antwort6,
+    data.antwort7,
+    data.antwort8,
+    data.antwort9,
+    data.antwort10,
+    data.antwort11,
+    data.antwort12,
+    data.antwort13,
+    data.antwort14,
+    data.antwort15,
+    data.antwort16,
+    data.antwort17
+  ];
+
+  /*
+  surveyData.a1 = data.antwort1;
+  surveyData.a2 = data.antwort2;
+  surveyData.a3 = data.antwort3;
+  surveyData.a4 = data.antwort4;
+  surveyData.a5 = data.antwort5;
+  surveyData.a6 = data.antwort6;
+  surveyData.a7 = data.antwort7;
+  surveyData.a8 = data.antwort8;
+  surveyData.a9 = data.antwort9;
+  surveyData.a10 = data.antwort10;
+  surveyData.a11 = data.antwort11;
+  surveyData.a12 = data.antwort12;
+  surveyData.a13 = data.antwort13; 
+  surveyData.a14 = data.antwort14; 
+  surveyData.a15 = data.antwort15; 
+  surveyData.a16 = data.antwort16;
+  surveyData.a17 = data.antwort17;
+  */
 }
 
 function draw() {
   frameRate(2);
-
   //video-tracking übergabe aufgabe on face-detection
   /*if (ctracker.getCurrentPosition()[0]) {
 console.log("chabis")
@@ -85,26 +128,21 @@ console.log("nix")
   loadJSON("data.json", drawData);
 
   //clearing canvas after generating visual
-  if(a1 === 0) return clear();
-  if(a1 === -1) return;
+  if(surveyData[0] === 0)
+  {
+    resetAllParameters();
+    return clear();
+  }
 
-  antwort1(a1);
-  antwort2(a2);
-  antwort3(a3);
-  antwort4(a4);
-  antwort5(a5);
-  antwort6(a6);
-  antwort7(a7);
-  antwort8(a8);
-  antwort9(a9);
-  antwort10(a10);
-  antwort11(a11);
-  antwort12(a12);
-  antwort13(a13);
-  antwort14(a14);
-  antwort15(a15);
-  antwort16(a16);
-  antwort17(a17);
+  if(surveyData[0] === -1)
+  {
+    resetAllParameters();
+    return;
+  }
+
+  overwriteEachAnswer();
+  visualize();
+  console.log(possibleParameters);
 }
 
 //create polygon
@@ -119,74 +157,163 @@ function polygon(x, y, radius, npoints) {
   endShape(CLOSE);
 }
 
+function overwriteEachAnswer()
+{
+  possibleParameters.generalAngle += possibleParameters.distance;
+  surveyData.forEach(
+    function handleAnswers(value, index, array)
+    {
+      if (typeof(value) != "undefined")
+      {
+        //console.log("surveyNumber " + index + " answer: " + value);
+        changeValuesBasedOnSurvey(index, value);
+      }
+    }
+  );
+}
 
-
-function filterMode() {
-  //a
-  if (keyCode === 65) {
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-    blendMode(MULTIPLY);
+function setParams(obj)
+{
+  if (typeof(obj.angle) != "undefined")
+  {
+    possibleParameters.angle = obj.angle;
   }
-
-  // s
-  else if (keyCode === 83) {
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-    blendMode(LIGHTEST);
+  if (typeof(obj.generalAngle) != "undefined")
+  {
+    possibleParameters.generalAngle = obj.generalAngle;
   }
-
-  //d
-  else if (keyCode === 68) {
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-    blendMode(DIFFERENCE);
+  if (typeof(obj.pos) != "undefined")
+  {
+    possibleParameters.pos = obj.pos;
   }
-
-  //m
-  else if (keyCode === 77) {
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-    blendMode(SCREEN);
+  if (typeof(obj.npoints) != "undefined")
+  {
+    possibleParameters.npoints = obj.npoints;
+  }
+  if (typeof(obj.radius) != "undefined")
+  {
+    possibleParameters.radius = obj.radius;
+  }
+  if (typeof(obj.distance) != "undefined")
+  {
+    possibleParameters.distance = obj.distance;
+  }
+  if (typeof(obj.colorR) != "undefined")
+  {
+    possibleParameters.colorR = obj.colorR;
+  }
+  if (typeof(obj.colorG) != "undefined")
+  {
+    possibleParameters.colorG = obj.colorG;
+  }
+  if (typeof(obj.colorB) != "undefined")
+  {
+    possibleParameters.colorB = obj.colorB;
+  }
+  if (typeof(obj.a) != "undefined")
+  {
+    possibleParameters.a = obj.a;
   }
 }
 
-
-
-function verschiebung() {
-  // u
-  if (keyCode === 85) {
-    pos = 200;
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-  }
-
-  //i
-  else if (keyCode === 73) {
-    pos = 150;
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-  }
-
-  //o
-  else if (keyCode === 79) {
-    pos = 50;
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-  }
-
-  // p
-  else if (keyCode === 80) {
-    pos = 80;
-    rotate(-generalAngle);
-    polygon(pos, pos, radius, npoints);
-    generalAngle = generalAngle + distance;
-  }
+function visualize()
+{
+  fill
+  (
+    possibleParameters.colorR,
+    possibleParameters.colorG,
+    possibleParameters.colorB,
+    possibleParameters.a
+  );
+  rotate(possibleParameters.generalAngle);
+  
+  polygon
+  (
+    possibleParameters.pos,
+    possibleParameters.pos,
+    possibleParameters.radius,
+    possibleParameters.npoints
+  );
 }
+
+function changeValuesBasedOnSurvey(surveyNumber, answerNumber)
+{
+  let changedParams = {};
+
+  switch(surveyNumber + 1) // 1 - 17
+  {
+    case 1:
+        switch(answerNumber) // 1 - 4 oder 6
+        {
+          case 1:
+            changedParams.colorR = random(140, 150);
+            changedParams.colorG = random(10, 30);
+            changedParams.colorB = random(120, 130);
+          break;
+          case 2:
+              changedParams.colorR = random(0, 0);
+              changedParams.colorG = random(10, 30);
+              changedParams.colorB = random(120, 130);
+          break;
+          case 3:
+          break;
+          case 4:
+          break;
+          case 5:
+          break;
+          case 6:
+          break;
+        }
+    break;
+    case 2:
+        switch(answerNumber) // 0 - 4 oder 6
+        {
+          case 1:
+              changedParams.pos = 140;
+              changedParams.radius = 140;
+          break;
+          case 2:
+              changedParams.pos = 120;
+              changedParams.radius = 120;
+          break;
+          case 3:
+          break;
+          case 4:
+          break;
+        }
+    break;
+    case 3:
+    break;
+    case 4:
+    break;
+    case 5:
+    break;
+    case 6:
+      switch(answerNumber) // 0 - 4 oder 6
+      {
+        case 1:
+          changedParams.colorR = random(0, 10);
+          changedParams.colorG = random(10, 30);
+          changedParams.colorB = random(200, 230);
+        break;
+        case 2:
+            changedParams.colorR = random(90, 100);
+            changedParams.colorG = random(100, 130);
+            changedParams.colorB = random(120, 130);
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+        break;
+      }
+    break;
+    default:
+    break;
+  }
+  setParams(changedParams);
+}
+
