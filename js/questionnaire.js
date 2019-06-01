cbe4ad31b4d953c8f06b858ee0f1516160e60b1f
lauragyr <laura.gyr@hotmail.com>
2019-06-02 00:08:57 +0200

    added functions for print-button

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -5,75 +5,72 @@ let startScreen = document.getElementById("homescreen");
 //  questionSwitch
 let questionSwitch = [];
 
-for (let i = 0; i < 15; i++)
-{
-    let questionString = "questionBox" + (i + 1);
-    let obj = document.getElementById(questionString);
-    questionSwitch.push(obj);
+for (let i = 0; i < 15; i++) {
+  let questionString = "questionBox" + (i + 1);
+  let obj = document.getElementById(questionString);
+  questionSwitch.push(obj);
 }
 
-questionSwitch.forEach(function (questionBox, index, arr)
-{
-    let possibleButtons = [];
-    possibleButtons = possibleButtons.concat([].slice.call(questionBox.getElementsByClassName("circle")));
-    possibleButtons = possibleButtons.concat([].slice.call(questionBox.getElementsByClassName("choiceImage")));
-    possibleButtons = possibleButtons.concat([].slice.call(questionBox.getElementsByClassName("choiceAnswer")));
+questionSwitch.forEach(function (questionBox, index, arr) {
+  let possibleButtons = [];
+  possibleButtons = possibleButtons.concat([].slice.call(questionBox.getElementsByClassName("circle")));
+  possibleButtons = possibleButtons.concat([].slice.call(questionBox.getElementsByClassName("choiceImage")));
+  possibleButtons = possibleButtons.concat([].slice.call(questionBox.getElementsByClassName("choiceAnswer")));
 
-    possibleButtons.forEach(function (button, buttonArrIndex)
-    {
-        if (index < arr.length)
-        {
-            button.addEventListener("click", function()
-            {
-                setTimeout(function() {
-                    arr[index].style.display = "none";
-                    arr[index + 1].style.display = "block";
-                }, 500);
-                
-            });
-        }
-    });
+  possibleButtons.forEach(function (button, buttonArrIndex) {
+    if (index < arr.length) {
+      button.addEventListener("click", function () {
+        setTimeout(function () {
+          arr[index].style.display = "none";
+          arr[index + 1].style.display = "block";
+        }, 500);
+
+      });
+    }
+  });
 });
 
-function end()
-{
-    socket.emit('answers', -1);
-    //socket.emit('answers', 0); //clear server
-    location.reload()
-} 
+function end() {
+  socket.emit('answers', -1);
+  location.reload()
+}
 
 //reload all content
-document.addEventListener("DOMContentLoaded", function ()
-{
-    socket.emit('answers', 0);
+//function restart() {
+document.addEventListener("DOMContentLoaded", function () {
+  socket.emit('answers', 0);
 });
+//}
 
-document.getElementById("start").addEventListener("click", function(){
-    setTimeout(function() {
-        socket.emit('answers', 0);
-        questionBox1.style.display = "block";
-        startScreen.style.display = "none";
-    }, 500);
-    start.className = "button activeButton";
+
+//start-button on homescreen
+document.getElementById("start").addEventListener("click", function () {
+  setTimeout(function () {
+    socket.emit('answers', 0);
+    questionBox1.style.display = "block";
+    startScreen.style.display = "none";
+  }, 500);
+  start.className = "button activeButton";
 })
 
-document.getElementById("backto").addEventListener("click", function(){
-    setTimeout(function() {
-        socket.emit('answers', -1);
-        startScreen.style.display = "block";
-        questionBox15.style.display = "none";
-    }, 500);
-    backto.style.border = "none";
-    backto.className = "endButton activeEnd";
-    start.className = "button";
+//restart-button on last page
+document.getElementById("backto").addEventListener("click", function () {
+  setTimeout(function end() {
+    startScreen.style.display = "block";
+    questionBox15.style.display = "none";
+  }, 500);
+  backto.style.border = "none";
+  backto.className = "endButton activeEnd";
+  start.className = "button";
 })
 
-document.getElementById("drucken").addEventListener("click", function(){
-    setTimeout(function() {
-        socket.emit('answers', -2);
-        //startScreen.style.display = "none";
-        //questionBox16.style.display = "block";
-    }, 500);
-    drucken.style.border = "none";
-    drucken.className = "printButton activeEnd";
+//print-button
+document.getElementById("drucken").addEventListener("click", function () {
+  setTimeout(function () {
+    socket.emit('answers', -2);
+    questionBox15.style.display = "none";
+    printBox.style.display = "block";
+  }, 500);
+  drucken.style.border = "none";
+  drucken.className = "printButton activeEnd";
 })
